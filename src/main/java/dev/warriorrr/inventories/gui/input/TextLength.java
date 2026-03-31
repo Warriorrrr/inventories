0a9db3dc9d1086558b47e597d695245412b16da1
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-03-31 17:18:08 +0200

    javadoc issue

--- src/main/java/dev/warriorrr/inventories/gui/input/TextLength.java
+++ src/main/java/dev/warriorrr/inventories/gui/input/TextLength.java
@@ -16,7 +16,7 @@ public record TextLength(@Range(from = 1, to = Integer.MAX_VALUE) int characters
     /**
      * @param characters The minimum number of needed characters
      * @return A new {@link TextLength} instance.
-     * @throws IllegalArgumentException if characters <= 0
+     * @throws IllegalArgumentException if characters &lt;= 0
      */
     public static TextLength ofAtLeast(final @Range(from = 1, to = Integer.MAX_VALUE) int characters) {
         Preconditions.checkArgument(characters > 0, "characters must be positive, got %s", characters);
