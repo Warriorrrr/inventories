7b1ac4d1e7b6fd7abc9bb4d472342976e9e4b0bd
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-06-30 15:27:06 +0200

    Collect builtin methods automatically to set

--- src/main/java/dev/warriorrr/inventories/gui/input/BuiltinInputMethods.java
+++ src/main/java/dev/warriorrr/inventories/gui/input/BuiltinInputMethods.java
@@ -4,14 +4,21 @@ import dev.warriorrr.inventories.gui.input.impl.dialog.DialogInputOptionsBuilder
 import dev.warriorrr.inventories.gui.input.impl.sign.SignInputOptionsBuilder;
 import dev.warriorrr.inventories.gui.input.impl.text.ChatInputOptionsBuilder;
 import net.kyori.adventure.key.Key;
+import org.jetbrains.annotations.Unmodifiable;
 
+import java.lang.reflect.Modifier;
 import java.util.Collection;
-import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * A class that holds the keys for builtin input methods.
  */
 public class BuiltinInputMethods {
+    private static final Collection<Key> KEYS;
+
+    private BuiltinInputMethods() {}
+
     /**
      * Key for the builtin chat input method.
      */
@@ -30,7 +37,20 @@ public class BuiltinInputMethods {
     /**
      * {@return the keys for all builtin input methods}
      */
-    public static Collection<Key> keys() {
-        return Set.of(CHAT.key(), SIGN.key(), DIALOG.key());
+    public static @Unmodifiable Collection<Key> keys() {
+        return KEYS;
+    }
+
+    static {
+        KEYS = Stream.of(BuiltinInputMethods.class.getFields())
+                .filter(field -> field.getType().equals(InputMethodKey.class) && Modifier.isStatic(field.getModifiers()))
+                .map(field -> {
+                    try {
+                        return ((InputMethodKey<?>) field.get(null)).key();
+                    } catch (IllegalAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .collect(Collectors.toUnmodifiableSet());
     }
 }
