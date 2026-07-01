e63328eb11dc3191adbf70b41f511a570914704b
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-07-01 10:01:05 +0200

    Remove internal annotation from dialog response view

--- src/main/java/dev/warriorrr/inventories/gui/input/impl/dialog/DialogResponse.java
+++ src/main/java/dev/warriorrr/inventories/gui/input/impl/dialog/DialogResponse.java
@@ -2,10 +2,10 @@ package dev.warriorrr.inventories.gui.input.impl.dialog;
 
 import io.papermc.paper.dialog.DialogResponseView;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.Nullable;
 
-public record DialogResponse(Player player, @ApiStatus.Internal DialogResponseView responseView) {
+@SuppressWarnings("UnstableApiUsage")
+public record DialogResponse(Player player, DialogResponseView responseView) {
     public @Nullable String getText(final String key) {
         return this.responseView.getText(key);
     }
