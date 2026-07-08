bfc7ac5b4a9871899b3e5f83461d507d0568e1b4
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2026-07-08 14:58:14 +0200

    Expand MenuItem builder API
    Allow creating builders with pre-created ItemStack instances & allow further mutating the final built item. The data component API is also used under the hood instead of ItemMeta.

--- src/main/java/dev/warriorrr/inventories/gui/MenuItem.java
+++ src/main/java/dev/warriorrr/inventories/gui/MenuItem.java
@@ -1,17 +1,16 @@
 package dev.warriorrr.inventories.gui;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.ItemLore;
+import io.papermc.paper.datacomponent.item.ResolvableProfile;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextDecoration;
 import dev.warriorrr.inventories.gui.action.ClickAction;
 import dev.warriorrr.inventories.gui.slot.Slot;
 import dev.warriorrr.inventories.gui.slot.anchor.SlotAnchor;
-import org.bukkit.Bukkit;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,8 +18,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+@SuppressWarnings("UnstableApiUsage") // data component api
 public class MenuItem {
     public static final @NotNull NamespacedKey PDC_KEY = Objects.requireNonNull(NamespacedKey.fromString("inventories:item"));
 
@@ -67,59 +68,52 @@ public class MenuItem {
         return new Builder(type);
     }
 
+    public static Builder builder(@NotNull Supplier<@NotNull ItemStack> itemSupplier) {
+        return new Builder(itemSupplier);
+    }
+
+    public static Builder builder(@NotNull ItemStack itemStack) {
+        return new Builder(() -> itemStack);
+    }
+
     public Builder builder() {
-        Builder builder = builder(itemStack.getType())
-                .slot(slot)
-                .size(itemStack.getAmount());
+        Builder builder = builder(itemStack)
+                .slot(slot);
 
-        final ItemMeta meta = itemStack.getItemMeta();
-        if (meta != null) {
-            if (meta.hasEnchantmentGlintOverride() && meta.getEnchantmentGlintOverride()) {
-                builder.withGlint();
-            }
-
-            final Component displayName = meta.displayName();
-            if (displayName != null) {
-                builder.name(displayName);
-            }
-
-            final List<Component> lore = meta.lore();
-            if (lore != null) {
-                builder.lore(lore);
-            }
-
-            if (itemStack.getType() == Material.PLAYER_HEAD && meta instanceof SkullMeta skullMeta && skullMeta.getPlayerProfile() instanceof PlayerProfile profile && profile.getId() != null) {
-                builder.skullOwner(profile.getId());
-            }
+        final ItemLore lore = itemStack.getData(DataComponentTypes.LORE);
+        if (lore != null) {
+            builder.loreBuilder = ItemLore.lore().addLines(lore.lines());
         }
 
-        for (ClickAction clickAction : this.actions)
+        for (ClickAction clickAction : this.actions) {
             builder.action(clickAction);
+        }
 
         return builder;
     }
 
     public static class Builder {
-        private final Material type;
-        private Component name = Component.empty();
-        private final List<Component> lore = new ArrayList<>(0);
-        private int size = 1;
+        private final ItemStack itemStack;
+        private ItemLore.Builder loreBuilder = null;
         private Slot slot = SlotAnchor.ofSlot(0);
         private final List<ClickAction> actions = new ArrayList<>(0);
-        private boolean glint;
-        private UUID ownerUUID;
+        private Consumer<ItemStack> postBuildConsumer = null;
 
         private Builder(Material type) {
-            this.type = type;
+            this.itemStack = ItemStack.of(type);
+        }
+
+        private Builder(Supplier<ItemStack> itemSupplier) {
+            this.itemStack = itemSupplier.get().clone();
         }
 
         public Builder name(@NotNull Component name) {
-            this.name = name.decoration(TextDecoration.ITALIC, false);
+            this.itemStack.setData(DataComponentTypes.ITEM_NAME, name);
             return this;
         }
 
         public Builder size(int size) {
-            this.size = size;
+            this.itemStack.setAmount(size);
             return this;
         }
 
@@ -144,17 +138,21 @@ public class MenuItem {
         }
 
         public Builder withGlint() {
-            this.glint = true;
+            this.itemStack.setData(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true);
             return this;
         }
 
         public Builder withGlint(boolean glint) {
-            this.glint = glint;
+            this.itemStack.setData(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, glint);
             return this;
         }
 
         public Builder lore(@NotNull Component lore) {
-            this.lore.add(lore.decoration(TextDecoration.ITALIC, false));
+            if (loreBuilder == null) {
+                loreBuilder = ItemLore.lore();
+            }
+
+            loreBuilder.addLine(lore.decorationIfAbsent(TextDecoration.ITALIC, TextDecoration.State.FALSE));
             return this;
         }
 
@@ -176,28 +174,30 @@ public class MenuItem {
         }
 
         public Builder skullOwner(@NotNull UUID ownerUUID) {
-            this.ownerUUID = ownerUUID;
+            this.itemStack.setData(DataComponentTypes.PROFILE, ResolvableProfile.resolvableProfile().uuid(ownerUUID).build());
+            return this;
+        }
+
+        /**
+         * Allows setting an {@link ItemStack} consumer for further modifying the built item.
+         *
+         * @param itemMutator The item consumer to set.
+         * @return {@code this}
+         */
+        public Builder mutateItem(Consumer<ItemStack> itemMutator) {
+            this.postBuildConsumer = itemMutator;
             return this;
         }
 
         public MenuItem build() {
-            ItemStack itemStack = new ItemStack(type, size);
+            itemStack.editPersistentDataContainer(pdc -> pdc.set(PDC_KEY, PersistentDataType.BYTE, (byte) 1));
 
-            ItemMeta meta = itemStack.getItemMeta();
-            if (meta != null) {
-                meta.getPersistentDataContainer().set(PDC_KEY, PersistentDataType.BYTE, (byte) 1);
+            if (this.loreBuilder != null) {
+                itemStack.setData(DataComponentTypes.LORE, this.loreBuilder.build());
+            }
 
-                meta.displayName(this.name);
-                meta.lore(lore);
-
-                if (meta instanceof SkullMeta skullMeta && this.ownerUUID != null)
-                    skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(this.ownerUUID));
-
-                if (glint) {
-                    meta.setEnchantmentGlintOverride(true);
-                }
-
-                itemStack.setItemMeta(meta);
+            if (postBuildConsumer != null) {
+                postBuildConsumer.accept(itemStack);
             }
 
             MenuItem item = new MenuItem(itemStack, slot);
